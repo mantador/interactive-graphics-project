f6fa51c3b5866bf5c910782e4733e24e9f963096
Odysseas Diamadis <odysseas@dilmun.net>
2024-10-27 22:31:26 +0100

    working simulation

--- lib.js
+++ lib.js
@@ -4,21 +4,17 @@
   var __publicField = (obj, key, value) => __defNormalProp(obj, typeof key !== "symbol" ? key + "" : key, value);
 
   // lib/constants.js
-  var _Constants = class _Constants {
-    static get resolution() {
-      return _Constants.canvasHeight / _Constants.canvasWidth;
-    }
-    static setCanvasDimensions(width, height) {
-      _Constants.canvasWidth = width;
-      _Constants.canvasHeight = height;
-    }
+  var Constants = class {
   };
-  __publicField(_Constants, "nBodies", 8);
-  __publicField(_Constants, "canvasWidth");
-  __publicField(_Constants, "canvasHeight");
-  __publicField(_Constants, "dt", 1);
-  __publicField(_Constants, "log", false);
-  var Constants = _Constants;
+  __publicField(Constants, "nBodies", 20);
+  __publicField(Constants, "dt", 1);
+  __publicField(Constants, "G", 0.05);
+  __publicField(Constants, "log", false);
+  __publicField(Constants, "minRadius", 10);
+  __publicField(Constants, "maxRadius", 30);
+  __publicField(Constants, "lightPosition", [300, 200, 1500]);
+  __publicField(Constants, "cameraPosition", [500, 500, 2e3]);
+  __publicField(Constants, "cameraTarget", [500, 500, 500]);
 
   // lib/sphere.js
   var _Sphere = class _Sphere {
@@ -125,9 +121,6 @@
     }
     checkGlExtensions(canvas);
     resizeCanvasToDisplaySize(canvas);
-    const cw = canvas.clientWidth;
-    const ch = canvas.clientHeight;
-    Constants.setCanvasDimensions(cw, ch);
     return canvas;
   }
 
@@ -138,7 +131,7 @@
   );
   var fsVelocities = (
     /*glsl*/
-    "\nprecision mediump float;\n\nuniform sampler2D positionTexture;\nuniform sampler2D velocityTexture;\nuniform vec2 dimensions;\nuniform float DT;\n\nconst float MAX_ITER=1000.0;\n\nvec2 indexToTextureIndex(vec2 dimensions, float index) {\n  float y = floor(index / dimensions.x);\n  float x = mod(index, dimensions.x);\n  return (vec2(x, y) + 0.5) / dimensions;\n}\n\nvec3 reflectVelocity(vec3 vel, vec3 normal) {\n  float dotp = dot(vel.xyz, normal);\n  float norm = dot(normal, normal);\n  return vel.xyz - (2.0 * (dotp/norm) )*normal;\n}\n\nvec3 checkAndAdjustCollision(vec4 pos, vec3 vel) {\n  if (pos.x >= 1000.0) {\n    vel = reflectVelocity(vel, vec3(-1, 0, 0));\n  }\n\n  if (pos.x <= 0.0) {\n    vel = reflectVelocity(vel, vec3(1, 0, 0));\n  }\n\n  if (pos.y >= 1000.0) { // Collision on upper plane\n    vel = reflectVelocity(vel, vec3(0, -1, 0));\n  }\n  if (pos.y <= 0.0) {\n    vel = reflectVelocity(vel, vec3(0, 1, 0));\n  }\n\n  if (pos.z >= 1000.0) {\n    vel = reflectVelocity(vel, vec3(0, 0, -1));\n  }\n\n  if (pos.z <= 0.0) {\n    vel = reflectVelocity(vel, vec3(0, 0, 1));\n  }\n\n  return vel;\n}\n\n\nvoid main() {\n  vec2 texcoord = gl_FragCoord.xy / dimensions;\n  vec3 force = vec3(0, 0, 0);\n  vec4 p1 = texture2D(positionTexture, texcoord);\n  vec4 v = texture2D(velocityTexture, texcoord);  \n  \n  float G = 10.0;\n\n  vec3 totalForce = vec3(0, 0, 0);\n\n  for(float i = 0.0; i < MAX_ITER; i++) {\n    if (float(i) == floor(gl_FragCoord.x)) { continue; }\n    if (float(i) == dimensions.x) { break; }\n\n    vec2 index = indexToTextureIndex(dimensions, float(i));\n\n    vec4 p2 = texture2D(positionTexture, index);\n    vec3 diff = p2.xyz - p1.xyz;\n    float distanc = pow(length(diff), 3.0);\n\n    vec3 force = G*( (p1.w * p2.w) / (distanc) )*diff;\n    totalForce += force;\n  }\n\n  vec3 acc = totalForce/p1.w;\n\n  vec3 deltaVel = acc*DT; // delta T\n  vec3 vel = v.xyz + deltaVel;\n  \n  vec3 newVel = checkAndAdjustCollision(p1, vel);\n\n  gl_FragColor = vec4(newVel, 0);\n}\n"
+    "\nprecision mediump float;\n\nuniform sampler2D positionTexture;\nuniform sampler2D velocityTexture;\nuniform vec2 dimensions;\nuniform float DT;\nuniform float G;\n\nconst float MAX_ITER=1000.0;\n\nvec2 indexToTextureIndex(vec2 dimensions, float index) {\n  float y = floor(index / dimensions.x);\n  float x = mod(index, dimensions.x);\n  return (vec2(x, y) + 0.5) / dimensions;\n}\n\nvec3 reflectVelocity(vec3 vel, vec3 normal) {\n  float dotp = dot(vel.xyz, normal);\n  float norm = dot(normal, normal);\n  return vel.xyz - (2.0 * (dotp/norm) )*normal;\n}\n\nvec3 checkAndAdjustWallCollisions(vec4 pos, vec3 vel) {\n  if (pos.x >= 1000.0) {\n    vel = reflectVelocity(vel, vec3(-1, 0, 0));\n  }\n\n  if (pos.x <= 0.0) {\n    vel = reflectVelocity(vel, vec3(1, 0, 0));\n  }\n\n  if (pos.y >= 1000.0) { // Collision on upper plane\n    vel = reflectVelocity(vel, vec3(0, -1, 0));\n  }\n  if (pos.y <= 0.0) {\n    vel = reflectVelocity(vel, vec3(0, 1, 0));\n  }\n\n  if (pos.z >= 1000.0) {\n    vel = reflectVelocity(vel, vec3(0, 0, -1));\n  }\n\n  if (pos.z <= 0.0) {\n    vel = reflectVelocity(vel, vec3(0, 0, 1));\n  }\n\n  return vel;\n}\n\nvec3 calculateCollisionVelocity(vec4 p1, vec4 p2, vec3 v1, vec3 v2) {\n  // p1.w and p2.w represent both mass and radius\n  // For proper physics, mass should scale with volume (r\xB3)\n  float m1 = p1.w * p1.w * p1.w;  // Mass of first sphere\n  float m2 = p2.w * p2.w * p2.w;  // Mass of second sphere\n  \n  // Calculate the collision normal\n  vec3 normal = normalize(p1.xyz - p2.xyz);\n  \n  // Calculate velocities along the normal\n  float v1n = dot(v1, normal);\n  float v2n = dot(v2, normal);\n  \n  // If spheres are moving apart, no collision needed\n  if (v1n - v2n > 0.0) {\n      return v1;\n  }\n  \n  // Calculate new velocity along normal using conservation of momentum\n  // and energy for a 1D collision\n  float newV1n = (m1 * v1n + m2 * v2n - m2 * (v1n - v2n)) / (m1 + m2);\n  \n  // Calculate the change in normal velocity\n  float deltaV1n = newV1n - v1n;\n  \n  // Apply the velocity change along the normal direction\n  // Keep the tangential velocity component unchanged\n  return v1 + deltaV1n * normal;\n}\n\n\nvec3 checkAndAdjustSphereCollisions(vec4 p1, vec3 v1) {\n  vec3 newVel = v1;\n  \n  for(float i = 0.0; i < MAX_ITER; i++) {\n      if (float(i) == floor(gl_FragCoord.x)) { continue; }\n      if (float(i) == dimensions.x) { break; }\n      \n      vec2 index = indexToTextureIndex(dimensions, float(i));\n      vec4 p2 = texture2D(positionTexture, index);\n      vec4 v2 = texture2D(velocityTexture, index);\n      \n      float distance = length(p1.xyz - p2.xyz);\n      float minDist = p1.w + p2.w;  // Sum of radii\n      \n      if (distance < minDist) {\n          // Collision detected\n          newVel = calculateCollisionVelocity(p1, p2, newVel, v2.xyz);\n          \n          // Add separation vector to prevent spheres from sticking\n          vec3 separationNormal = normalize(p1.xyz - p2.xyz);\n          float overlap = minDist - distance;\n          newVel += separationNormal * overlap * 0.1; // Scale factor to prevent too strong separation\n      }\n  }\n  \n  return newVel;\n}\n\n\nvoid main() {\n  vec2 texcoord = gl_FragCoord.xy / dimensions;\n  vec3 force = vec3(0, 0, 0);\n  vec4 p1 = texture2D(positionTexture, texcoord);\n  vec4 v = texture2D(velocityTexture, texcoord);  \n  \n  vec3 totalForce = vec3(0, 0, 0);\n  float m1 = pow(p1.w, 3.0);\n  \n  for(float i = 0.0; i < MAX_ITER; i++) {\n    if (float(i) == floor(gl_FragCoord.x)) { continue; }\n    if (float(i) == dimensions.x) { break; }\n    \n    vec2 index = indexToTextureIndex(dimensions, float(i));\n    \n    vec4 p2 = texture2D(positionTexture, index);\n    float m2 = pow(p2.w, 3.0);\n    vec3 diff = p2.xyz - p1.xyz;\n    float distanc = pow(length(diff), 3.0);\n\n    vec3 force = G*( (m1 * m2) / (distanc) )*diff;\n    totalForce += force;\n  }\n\n  vec3 acc = totalForce/m1;\n\n  vec3 deltaVel = acc*DT; // delta T\n  vec3 vel = v.xyz + deltaVel;\n  \n  vec3 newVel;\n  newVel = checkAndAdjustSphereCollisions(p1, vel);\n  newVel = checkAndAdjustWallCollisions(p1, newVel);\n\n  gl_FragColor = vec4(newVel, 0);\n}\n"
   );
   var vsPositions = (
     /*glsl*/
@@ -169,7 +162,8 @@
       positionTexture: gl.getUniformLocation(velocityProgram, "positionTexture"),
       velocityTexture: gl.getUniformLocation(velocityProgram, "velocityTexture"),
       dimensions: gl.getUniformLocation(velocityProgram, "dimensions"),
-      dt: gl.getUniformLocation(velocityProgram, "DT")
+      dt: gl.getUniformLocation(velocityProgram, "DT"),
+      G: gl.getUniformLocation(velocityProgram, "G")
     };
     const spherePositions = new Float32Array(
       spheres.map((sphere) => sphere.center).flat()
@@ -249,6 +243,7 @@
       gl.uniform1i(velocityProgramLocs.velocityTexture, 1);
       gl.uniform2f(velocityProgramLocs.dimensions, textureWidth, textureHeight);
       gl.uniform1f(velocityProgramLocs.dt, Constants.dt);
+      gl.uniform1f(velocityProgramLocs.G, Constants.G);
       gl.bindFramebuffer(gl.FRAMEBUFFER, dataBuffer.velocity.frameBuffer);
       gl.viewport(0, 0, textureWidth, textureHeight);
       gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
@@ -378,11 +373,11 @@
   function initGraphicsProgram(gl, spheres) {
     const vs = (
       /*glsl*/
-      "\n  const mediump float;\n  attribute float index;\n  varying vec3 normal;\n  uniform sampler2D positionTexture;\n  uniform vec2 dimensions;\n  uniform mat4 matrix;\n  uniform vec3 cameraPosition;\n\n  vec2 indexToTextureIndex(vec2 dimensions, float index) {\n    float y = floor(index / dimensions.x);\n    float x = mod(index, dimensions.x);\n    return (vec2(x, y) + 0.5) / dimensions;\n  }\n\n  void main() {\n    vec4 position = texture2D(positionTexture, indexToTextureIndex(dimensions, index));\n    gl_Position = matrix*vec4(position.xyz, 1.0);\n    \n    vec4 viewSpace = vec4(position.xyz - cameraPosition, 1.0);\n    float distanceFromCamera = length(viewSpace.xyz);\n\n\n    float baseSize = position.w;\n    float perspectiveScale = 1000.0 / distanceFromCamera;\n    gl_PointSize = baseSize * perspectiveScale;\n  }\n  "
+      "\n  const mediump float;\n  attribute float index;\n  varying vec3 normal;\n  uniform sampler2D positionTexture;\n  uniform vec2 dimensions;\n  uniform mat4 matrix;\n  uniform vec3 cameraPosition;\n  \n  varying vec3 vPos;\n  varying float vRad;\n  varying vec3 cameraPos;\n  \n  vec2 indexToTextureIndex(vec2 dimensions, float index) {\n    float y = floor(index / dimensions.x);\n    float x = mod(index, dimensions.x);\n    return (vec2(x, y) + 0.5) / dimensions;\n  }\n\n  void main() {\n    vec4 position = texture2D(positionTexture, indexToTextureIndex(dimensions, index));\n    vPos = position.xyz;\n    vRad = position.w;\n    cameraPos = cameraPosition;\n    \n    gl_Position = matrix*vec4(position.xyz, 1.0);\n    vec4 viewSpace = vec4(position.xyz - cameraPosition, 1.0);\n    float distanceFromCamera = length(viewSpace.xyz);\n    \n    float baseSize = position.w;\n    float perspectiveScale = 1000.0 / distanceFromCamera;\n    gl_PointSize = baseSize * perspectiveScale;\n  }\n  "
     );
     const fs = (
       /*glsl*/
-      "\n  precision mediump float;\n  void main() {\n      vec2 centerToPixel = 2.0 * gl_PointCoord - 1.0;\n      if (dot(centerToPixel, centerToPixel) > 1.0) {\n          discard;\n      }\n      \n      gl_FragColor = vec4(1.0, 1.0, 1.0, 1.0); \n  }\n  "
+      "\n  precision mediump float;\n\n  varying vec3 vPos;\n  varying float vRad;\n  varying vec3 cameraPos;\n  \n  uniform vec3 lightPos;\n\n  void main() {\n    vec2 centerToPixel = 2.0 * gl_PointCoord - 1.0;\n    float r2 = dot(centerToPixel, centerToPixel);\n    if (dot(centerToPixel, centerToPixel) > 1.0) {\n        discard;\n      }\n      \n    float z = sqrt(1.0 - r2);\n    vec3 normal = normalize(vec3(centerToPixel.x, -centerToPixel.y, z));\n\n    vec3 fragPos = vPos + vRad * normal;\n    vec3 lightDir = normalize(lightPos - fragPos);\n    vec3 viewDir = normalize(cameraPos - fragPos);\n    vec3 halfDir = normalize(lightDir + viewDir);\n    \n    float ambient = 0.2;\n    \n    float diff = max(dot(normal, lightDir), 0.0);\n    \n    float spec = pow(max(dot(normal, halfDir), 0.0), 2.0);\n    \n    vec3 sphereColor = vec3(1.0, 1.0, 1.0);\n    vec3 finalColor = sphereColor * (ambient + diff + 0.5 * spec);\n\n    gl_FragColor = vec4(finalColor, 1.0);\n  }\n  "
     );
     const program = createWebglProgram(gl, vs, fs);
     gl.useProgram(program);
@@ -391,15 +386,18 @@
     const dimensionsLoc = gl.getUniformLocation(program, "dimensions");
     const matrixLoc = gl.getUniformLocation(program, "matrix");
     const cameraLoc = gl.getUniformLocation(program, "cameraPosition");
+    const lightLoc = gl.getUniformLocation(program, "lightPos");
     gl.uniform1i(textureLoc, 0);
     gl.uniform2f(dimensionsLoc, spheres.length, 1);
+    const lightPosition = Constants.lightPosition;
+    gl.uniform3f(lightLoc, lightPosition[0], lightPosition[1], lightPosition[2]);
     if (gl.canvas instanceof OffscreenCanvas) {
       throw new Error("Nope...");
     }
     var aspect = gl.canvas.clientWidth / gl.canvas.clientHeight;
     var projectionMatrix = m4.perspective(60 * Math.PI / 180, aspect, 1, 2e3);
-    var cameraPosition = [500, 500, 2e3];
-    var target = [500, 500, 500];
+    var cameraPosition = Constants.cameraPosition;
+    var target = Constants.cameraTarget;
     var up = [0, 1, 0];
     var cameraMatrix = m4.lookAt(cameraPosition, target, up, m4.identity());
     var viewMatrix = m4.inverse(cameraMatrix);
@@ -412,6 +410,12 @@
     gl.uniform3f(cameraLoc, cameraPosition[0], cameraPosition[1], cameraPosition[2]);
     const pBuffer = gl.createBuffer();
     let ids = new Array(spheres.length).fill(0).map((_, i) => i);
+    gl.bindBuffer(gl.ARRAY_BUFFER, pBuffer);
+    gl.bufferData(
+      gl.ARRAY_BUFFER,
+      new Float32Array(ids),
+      gl.STATIC_DRAW
+    );
     return {
       render: (dataBuffer) => {
         gl.useProgram(program);
@@ -419,11 +423,6 @@
         gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
         gl.enable(gl.DEPTH_TEST);
         gl.bindBuffer(gl.ARRAY_BUFFER, pBuffer);
-        gl.bufferData(
-          gl.ARRAY_BUFFER,
-          new Float32Array(ids),
-          gl.STATIC_DRAW
-        );
         gl.enableVertexAttribArray(indexLoc);
         gl.vertexAttribPointer(indexLoc, 1, gl.FLOAT, false, 0, 0);
         gl.activeTexture(gl.TEXTURE0);
@@ -441,10 +440,20 @@
     const canvas = initCanvas();
     const gl = canvas.getContext("webgl");
     if (!gl) throw new Error("WebGL not enabled!");
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
     const spheres = [];
-    for (let i = 0; i < 10; i++) {
-      spheres.push(Sphere.random(10));
+    for (let i = 0; i < Constants.nBodies; i++) {
+      spheres.push(Sphere.random());
     }
+    var aspect = gl.canvas.clientWidth / gl.canvas.clientHeight;
+    var projectionMatrix = m4.perspective(60 * Math.PI / 180, aspect, 1, 2e3);
+    var cameraPosition = Constants.cameraPosition;
+    var target = Constants.cameraTarget;
+    var up = [0, 1, 0];
+    var cameraMatrix = m4.lookAt(cameraPosition, target, up, m4.identity());
+    var viewMatrix = m4.inverse(cameraMatrix);
+    var viewProjectionMatrix = m4.multiply(projectionMatrix, viewMatrix);
     const counter = new Counter();
     const compute = initComputingProgram(gl, spheres);
     const render = initGraphicsProgram(gl, spheres);
